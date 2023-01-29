6df4a9101c5e164c4606500ca684699b77343ff1
Kristian Popov <kristianpopov03@yahoo.com>
2023-01-29 16:50:21 +0100

    Add files via upload

---
+++ Figur/Triangle(opposite)/Imposible Figur(triangle)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Imposible_Figur_triangle_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 20; j++)
+                {
+                    if (j > i && j < 10 + (10 - i))
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine("");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
