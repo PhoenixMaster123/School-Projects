cfcc93419112497de5101da6a1b5771e7012dbc8
Kristian Popov <kristianpopov03@yahoo.com>
2023-01-29 18:01:03 +0200

    Figur - Triangle

--- Figur/Triangle(opposite)/Imposible Figur(triangle)/Program.cs
+++ Figur/Triangle(opposite)/Imposible Figur(triangle)/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 
-namespace Imposible_Figur_triangle_
+namespace Triangle
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
             for (int i = 1; i <= 10; i++)
             {
                 for (int j = 1; j <= 20; j++)
